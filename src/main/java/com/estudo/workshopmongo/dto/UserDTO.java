1b04a5b4e30a6e52de5f906061c3fd354f7542cb
Esdras Victor <toyobr@hotmail.com>
2021-01-19 10:00:02 -0300

    Classes dtos

---
+++ src/main/java/com/estudo/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,58 @@
+package com.estudo.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.estudo.workshopmongo.domain.User;
+
+public class UserDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+		
+	}
+
+	public UserDTO(String id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	
+	public UserDTO(User user) {
+		this.id = user.getId();
+		this.name = user.getName();
+		this.email = user.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/com/estudo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/estudo/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.estudo.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudo.workshopmongo.domain.User;
+import com.estudo.workshopmongo.dto.UserDTO;
 import com.estudo.workshopmongo.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> users = userService.findAll();
-		return ResponseEntity.ok().body(users);
+		List<UserDTO> userDTO = users.stream().map(m -> new UserDTO(m)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(userDTO);
 	}
 	
 }
