254e7adb712ec5c6f6bb6fb3aff377d264ee275d
Esdras Victor <toyobr@hotmail.com>
2021-01-19 23:42:00 -0300

    Delete User by Id

--- src/main/java/com/estudo/workshopmongo/services/UserService.java
+++ src/main/java/com/estudo/workshopmongo/services/UserService.java
@@ -38,4 +38,10 @@ public class UserService {
 		return new User(userDTO.getId(), userDTO.getName(),userDTO.getEmail());
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+		
+	}
+	
 }
