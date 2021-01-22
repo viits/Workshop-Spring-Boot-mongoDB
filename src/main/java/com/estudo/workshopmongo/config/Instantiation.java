346cf1c70bd0725badd4461418b74a47305e1718
Esdras Victor <toyobr@hotmail.com>
2021-01-22 10:37:22 -0300

    Referenciando os Posts no User

--- src/main/java/com/estudo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/estudo/workshopmongo/config/Instantiation.java
@@ -29,7 +29,7 @@ public class Instantiation  implements CommandLineRunner {
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
-		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null,"Maria Brown", "maria@gmail.com");
 		User alex = new User(null,"Alex Green", "alex@gmail.com");
@@ -41,6 +41,9 @@ public class Instantiation  implements CommandLineRunner {
 		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 		
 		
 		
--- src/main/java/com/estudo/workshopmongo/domain/User.java
+++ src/main/java/com/estudo/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.estudo.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		
@@ -49,6 +55,15 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -74,6 +89,8 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 	
 		
